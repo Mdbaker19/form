493b08bad6e6ba81c455fb633f50179e1d6dbd24
Matthew <matthew.d.baker20@gmail.com>
2020-12-19 16:13:33 -0600

    added methods to validate passwords and email address

--- form.js
+++ form.js
@@ -1,3 +1,32 @@
 $(document).ready(function (){
 
+    $("#submit").on("click", function (){
+       let password = $("#password1");
+       let validateP = $("#password2");
+        console.log(password.val());
+        console.log(validateP.val());
+        console.log(comparePasswords(password.val(), validateP.val()));
+        let email = $("#email");
+        console.log(validateEmail(email.val()));
+
+
+    });
+
+    function validateEmail(input){
+        let com = input.substring(input.length -4);
+        return (com === ".com" && input.includes("@"));
+    }
+
+    function comparePasswords(p1, p2){
+        if(p1.length < 1){
+            return false;
+        }
+        for(let i = 0; i < p1.length; i++){
+            if(p1[i] !== p2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
 });
